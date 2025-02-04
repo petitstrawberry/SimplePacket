7504442dbb8128c9c5737142640709102c851ea3
PetitStrawberry <deloreantaro@yahoo.co.jp>
2025-02-04 17:52:07 +0900

    add: EOF, comment

--- Sources/SimplePacket/Frame.swift
+++ Sources/SimplePacket/Frame.swift
@@ -7,15 +7,28 @@
 
 import Foundation
 
+/// Frame is a collection of packets
+/// A frame is a sequence of Packet objects.
+/// The frame is terminated by an EOF packet.
 public typealias Frame = [Packet]
 
+/// FrameDecoderError is an error that occurs when decoding a frame
 public enum FrameDecoderError: Error {
+    /// The frame is invalid
     case invalidFrame
+    /// The payload is invalid
     case invalidPayload
+    /// The EOF packet is not found
+    case eofNotFound
 }
 
 extension Frame {
-
+    /// Decode a frame from data
+    /// - Parameter data: The data to decode
+    /// - Returns: The decoded frame
+    /// - Throws: FrameDecoderError.invalidFrame if the frame is invalid
+    /// - Throws: FrameDecoderError.invalidPayload if the payload is invalid
+    /// - Note: The frame is must be terminated by an EOF packet
     public static func decode(from data: Data) throws -> Frame {
         var data = data
         var packets: [Packet] = []
@@ -44,6 +57,11 @@ extension Frame {
         return packets
     }
 
+    /// Encode the frame to data
+    /// - Returns: The encoded data
+    /// - Throws: An error if the frame cannot be encoded
+    /// - Note: The frame is terminated by an EOF packet
+    /// - Note: This method is the inverse of `decode(from:)`
     public func encode() throws -> Data {
         var data = Data()
         for packet in self {
@@ -52,7 +70,10 @@ extension Frame {
             data.append(UInt8((packet.length >> 8) & 0xff))
             data.append(packet.payload)
         }
-        data.append(0)
+        let eof = Packet.EOF
+        data.append(eof.type)
+        data.append(UInt8(eof.length & 0xff))
+        data.append(UInt8((eof.length >> 8) & 0xff))
         return data
     }
 }
--- Sources/SimplePacket/Packet.swift
+++ Sources/SimplePacket/Packet.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+/// Packet is a unit of data
+/// A packet is a object with the following structure:
+/// - 1 byte for the type
+/// - 2 bytes for the length (16bit unsigned integer in little-endian byte order)
+/// - N bytes for the payload
 public struct Packet: Equatable {
     public let type: UInt8
     public let length: UInt16
@@ -16,5 +21,16 @@ public struct Packet: Equatable {
         self.type = type
         self.length = UInt16(payload.count)
         self.payload = payload
+    }
+
+    /// EOF is a special packet type that indicates the end of the frame
+    /// It is used to terminate the frame
+    /// The type of EOF is 0
+    /// The length of EOF is 0
+    /// The payload of EOF is empty
+    /// - Returns: A packet with type 0, length 0, and empty payload
+    /// - Note: This is a convenience method to create an EOF packet
+    public static var EOF: Packet {
+        return Packet(type: 0, payload: Data())
     }
 }
